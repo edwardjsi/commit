b6c17fabf6c0547836a1c2d2494693bd605f0729
Edward Santosh <edwardjsi@gmail.com>
2025-12-17 18:24:45 +0530

    feat: add reusable VPC Terraform module

---
+++ Day39/infra/terraform/modules/vpc/main.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(
+    { Name = var.vpc_name },
+    var.tags
+  )
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    { Name = "${var.vpc_name}-public-${count.index + 1}" },
+    var.tags
+  )
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = merge(
+    { Name = "${var.vpc_name}-private-${count.index + 1}" },
+    var.tags
+  )
+}
---
+++ Day39/infra/terraform/modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
+  default     = {}
+}
